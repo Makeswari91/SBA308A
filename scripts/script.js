92e9a84466ee9144f2e9811c83610f21be135934
Makeswari Ramanathan <mahithamakeswari91@gmail.com>
2025-08-26 16:43:53 -0400

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+const apiKey = '6a02b5e57d5d17c6d0d96e7319cf1d88';
+const city = 'Chennai';
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=Chennai&appid=6a02b5e57d5d17c6d0d96e7319cf1d88&units=metric`)
+  .then(response => response.json())
+  .then(data => {
+    const weather = data.weather[0].description;
+    const temp = data.main.temp;
+    document.getElementById('weather').innerHTML = `
+      <h2>Weather in ${city}</h2>
+      <p>${weather}, ${temp}°C</p>
+    `;
+  })
+  .catch(error => console.error('Error fetching weather data:', error));
